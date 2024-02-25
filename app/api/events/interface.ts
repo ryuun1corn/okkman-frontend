4750d4b4c6e408ef7ce7bbc04bc12528f5ca2db3
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-02-25 16:01:27 +0700

    add: PATCH method for /events
    Added the PATCH method to update event.
    Requires trickier manipulation of Zod, but it works.

--- app/api/events/interface.ts
+++ app/api/events/interface.ts
@@ -21,3 +21,43 @@ export const createEventSchema = z.object({
 export const deleteEventSchema = z.object({
   id: z.number(),
 });
+
+export const updateEventSchema = z.object({
+  id: z.number(),
+  update: z
+    .object({
+      name: z.string().min(1).max(255).optional(),
+      start_date: z
+        .string()
+        .refine((data) => !isNaN(Date.parse(data)), {
+          message: "Invalid date format",
+        })
+        .transform((data) => new Date(data))
+        .optional(),
+      end_date: z
+        .string()
+        .refine((data) => !isNaN(Date.parse(data)), {
+          message: "Invalid date format",
+        })
+        .transform((data) => new Date(data))
+        .optional(),
+      location: z.string().min(1).max(255).optional(),
+      description: z.string().optional(),
+    })
+    .refine(
+      (data) => {
+        const hasProp = Object.keys(data).some((key) =>
+          [
+            "name",
+            "start_date",
+            "end_date",
+            "location",
+            "description",
+          ].includes(key)
+        );
+
+        return hasProp;
+      },
+      { message: "Needs at least one property to be updated" }
+    ),
+});
--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,10 @@
 import prisma from "@/prisma/client";
 import { NextResponse } from "next/server";
-import { createEventSchema, deleteEventSchema } from "./interface";
+import {
+  createEventSchema,
+  deleteEventSchema,
+  updateEventSchema,
+} from "./interface";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 import { handleZodErrors } from "../utility";
@@ -66,7 +70,10 @@ export async function DELETE(request: Request) {
       },
     });
 
-    return NextResponse.json(res);
+    return NextResponse.json({
+      message: "Successfully deleted the event",
+      deleted_event: res,
+    });
   } catch (error) {
     if (error instanceof PrismaClientKnownRequestError) {
       return NextResponse.json(
@@ -84,6 +91,45 @@ export async function DELETE(request: Request) {
   }
 }
 
-// export async function PATCH() {
-//   return;
-// }
+export async function PATCH(request: Request) {
+  const body = await request.json();
+  const validation = updateEventSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(
+      {
+        message: "Validation error",
+        errors: handleZodErrors(validation.error.errors),
+      },
+      {
+        status: 400,
+      }
+    );
+
+  try {
+    const res = await prisma.event.update({
+      where: {
+        id: validation.data.id,
+      },
+      data: validation.data.update,
+    });
+
+    return NextResponse.json({
+      message: "Successfully updated the event",
+      updated_event: res,
+    });
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      return NextResponse.json(
+        { message: "There is no event with the specified ID number." },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      {
+        message:
+          "An unexpected error occurred on the server. Please contact the developer.",
+      },
+      { status: 500 }
+    );
+  }
+}
