3e29abf29ba1bafb26f3d8c6387b812b2817e7a3
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-02-28 06:52:07 +0700

    refactor: updated number field in schema
    Added the requirements for the 'number' field to be positive and be an integer.

--- app/api/groups/mentor/schema.ts
+++ app/api/groups/mentor/schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const createMentorAndGroupSchema = z.object({
-  number: z.number().positive(),
+  number: z.number().positive().int().lte(999),
   mentor_name: z.string().min(1).max(255),
 });
--- app/api/groups/schema.ts
+++ app/api/groups/schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const createGroupSchema = z.object({
-  number: z.number(),
+  number: z.number().positive().int().lte(999),
   mentor_id: z.number(),
 });
