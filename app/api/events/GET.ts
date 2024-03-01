2b507f7d50a5da58d1d5036e330101da4a42aae9
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-03-02 03:10:46 +0700

    refactor: updated to show sponsors and speakers field

--- app/api/events/GET.ts
+++ app/api/events/GET.ts
@@ -2,7 +2,12 @@ import prisma from "@/prisma/client";
 import { NextResponse } from "next/server";
 
 export async function getAllEvents() {
-  const res = await prisma.event.findMany();
+  const res = await prisma.event.findMany({
+    include: {
+      sponsors: true,
+      speakers: true,
+    },
+  });
 
   return NextResponse.json({
     message: "Successfully get all events.",
